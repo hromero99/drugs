2ac34f401cb84327cc3ad03cd88829d6098226f2
hromerolopez <hromerolopez@protonmail.com>
2021-03-01 23:46:16 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# the_drugs
+
+Application to control the drug pattern
---
+++ android/app/src/main/kotlin/com/example/the_drugs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.the_drugs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
